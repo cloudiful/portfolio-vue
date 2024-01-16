f7f12260bf20d7cbdfd8cff4d160f1dd01ca1ba9
cloudiful <cloudiful@outlook.com>
2024-01-16 16:27:58 +0800

    add source link to footer

--- src/layouts/default/Footer.vue
+++ src/layouts/default/Footer.vue
@@ -3,13 +3,17 @@ import router from "@/router";
 
 const links = [
   '首页',
-  // '个人资料',
+  '网页源码',
   // '全部作品',
   // '联系方式',
 ]
 
-function goHome() {
-  router.replace({path: '/'})
+function go(s: string) {
+  if (s == '首页') {
+    router.replace({path: '/'})
+  } else if (s == '网页源码') {
+    window.open('https://github.com/cloudiful/portfolio-vue');
+  }
 }
 
 </script>
@@ -21,14 +25,14 @@ function goHome() {
         v-for="link in links"
         :key="link"
         variant="text"
-        class="mx-2"
+        class="mx-1"
         rounded="xl"
-        @click="goHome"
+        @click="go(link)"
       >
         {{ link }}
       </v-btn>
-      <v-col class="text-center mt-4" cols="12" style="padding-bottom: 6%">
-        {{ new Date().getFullYear() }} — <strong>Cloudiful</strong>
+      <v-col class="text-center mt-1" cols="12" style="padding-bottom: 6%">
+        <h5>{{ new Date().getFullYear() }} — <strong>Cloudiful</strong></h5>
       </v-col>
     </v-row>
   </v-footer>
