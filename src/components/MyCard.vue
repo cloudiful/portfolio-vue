73cf6d6d3b04cae41372ee9391c15542b0439e3e
cloudiful <cloudiful@outlook.com>
2024-01-19 15:50:38 +0800

    add MyCard component

---
+++ src/components/MyCard.vue
@@ -0,0 +1,132 @@
+<script setup lang="ts">
+
+import {ref} from "vue";
+import {Link} from "@/interfaces/link";
+
+withDefaults(defineProps<{
+  'imgName'?: string | null
+  'links'?: Array<Link> | null
+  'expandable'?: boolean
+}>(), {
+  imgName: '',
+  links: null,
+  expandable: true,
+})
+
+let show = ref(false);
+let showBtn = ref(false);
+
+function toggleDescription() {
+  show.value = !show.value;
+}
+
+function imgPathProcess(i: string | null) {
+  return new URL(`/src/assets/${i}.webp`, import.meta.url).href
+}
+
+function goLink(url: string) {
+  window.open(url);
+}
+
+</script>
+
+<template>
+  <v-card variant="text" class="mx-auto music-card"
+          @mouseenter="showBtn = true"
+          @mouseleave="showBtn = false">
+    <v-img @click="toggleDescription()"
+           class="album-cover"
+           :src="imgPathProcess(imgName)">
+    </v-img>
+
+    <v-expand-transition>
+
+      <div v-if="show">
+
+        <div class="expanded-area">
+          <v-btn
+            v-for="(item,i) in links"
+            :key="'link_'+i"
+            @click="goLink(item.link_url)"
+            variant="text"
+            rounded
+            class="music-link-btn"
+          >
+            {{ item.link_name }}
+          </v-btn>
+        </div>
+      </div>
+    </v-expand-transition>
+
+  </v-card>
+  <v-btn :ripple="false" class="show-more-btn" variant="plain" @click="toggleDescription()">
+    <Transition name="fade">
+      <v-icon v-if="showBtn"
+              size="large"
+              :icon="show ? 'mdi-chevron-up' : 'mdi-chevron-down'"></v-icon>
+    </Transition>
+  </v-btn>
+</template>
+
+<style scoped>
+.album-cover {
+  border-radius: 3vh;
+}
+
+.music-card {
+  max-width: 60vh;
+  background: none;
+  border-radius: 3vh;
+  margin-top: 4%;
+  margin-bottom: 4%;
+  transition: 400ms;
+}
+
+.music-card:hover {
+  box-shadow: 0 0 32px 12px rgba(131, 131, 131, 0.25);
+}
+
+.expanded-area {
+  width: 100%;
+  padding-top: 4%;
+  padding-bottom: 4%;
+}
+
+.show-more-btn {
+  width: 100%;
+  background: none;
+}
+
+.album-cover {
+  border-radius: 3vh;
+}
+
+.music-card {
+  max-width: 60vh;
+  background: none;
+  border-radius: 3vh;
+  margin-top: 4%;
+  margin-bottom: 4%;
+  transition: 400ms;
+}
+
+.music-card:hover {
+  box-shadow: 0 0 32px 12px rgba(131, 131, 131, 0.25);
+}
+
+.music-link-btn {
+  width: 80%;
+  height: 100%;
+  margin: 2%;
+  padding-top: 5%;
+  padding-bottom: 5%;
+  transition: 200ms;
+  border: 2px solid transparent;
+}
+
+.music-link-btn:hover {
+  transform: scale(1.05);
+  border: 2px solid rgba(128, 128, 128, 0.5);
+}
+
+</style>
