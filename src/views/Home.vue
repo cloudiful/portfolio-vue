0f98309cc3cbab3b1dab62e2ff230734d2761318
cloudiful <cloudiful@outlook.com>
2024-01-19 15:50:07 +0800

    delete MainPage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,18 @@
 <template>
-  <MainPage />
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+
+      <MyInfo></MyInfo>
+      <!--      <NewRelease></NewRelease>-->
+      <AllMusic style="margin-top: 10%"></AllMusic>
+      <AllGames></AllGames>
+
+    </v-responsive>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
-import MainPage from "@/components/MainPage.vue";
+import MyInfo from "@/components/MyInfo.vue";
+import AllMusic from "@/components/AllMusic.vue";
+import AllGames from "@/components/AllGames.vue";
 </script>
